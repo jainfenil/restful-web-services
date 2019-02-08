318283620a3ef72b783105c5a1e4c52ef4a82c21
fenil <jainfenil@gmail.com>
2019-02-07 23:29:46 -0600

    Versioning using content header

--- src/main/java/com/springbootVersionOne/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
+++ src/main/java/com/springbootVersionOne/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
@@ -3,8 +3,20 @@ package com.springbootVersionOne.rest.webservices.restfulwebservices.versioning;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/*
+* Factors to consider which method should we use for versioning
+* URI Pollution
+* Misuse of HTTP headers
+* Caching
+* Can we execute the request on te browser?
+* API documentation
+* */
+
+
 @RestController
 public class PersonVersioningController {
+
+    //Versioning using uri
 
     @GetMapping("v1/person")
     public PersonV1 personV1(){
@@ -37,6 +49,21 @@ public class PersonVersioningController {
 
     @GetMapping(value = "/person/header", headers = "X_API_VERSION=2")
     public PersonV2 headerV2(){
+        return new PersonV2(new Name("John"," Wick"));
+    }
+
+    /*
+    Versioning using producers
+    This is also called as "accept header" or "content negotiation"
+     */
+
+    @GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 producesV1(){
+        return new PersonV1("John Wick");
+    }
+
+    @GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v2+json")
+    public PersonV2 producesV2(){
         return new PersonV2(new Name("John"," Wick"));
     }
 }
